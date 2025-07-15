c7de6b907bef23a6d192d8102d9e20253e3ac1e0
GatekoNeko141 <jhon1995jera@gmail.com>
2025-07-15 13:34:38 -0500

    Llamado a las tarjetas y los tipos de datos

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 
 import Image from "next/image";
 import st from "@/app/page.module.scss";
+import CardProject from "./components/CardProject";
 
 export const metadata: Metadata = {
   title: "Portafolio de John Rodríguez Arteaga",
@@ -9,7 +10,7 @@ export const metadata: Metadata = {
 };
 
 const Home = () => {
-  const data = {
+  const data: Data = {
     image: 'https://picsum.photos/1000/1000',
     nombre: "John Eymar",
     apellido: "Rodríguez Arteaga",
@@ -72,10 +73,85 @@ const Home = () => {
       },
     ],
   }
+  
+  const proyects:Proyect[] = [
+    {
+      title: "Proyecto 1",
+      description: "Descripción del proyecto 1.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+    {
+      title: "Proyecto 2",
+      description: "Descripción del proyecto 2.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+    {
+      title: "Proyecto 3",
+      description: "Descripción del proyecto 3.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+    {
+      title: "Proyecto 4",
+      description: "Descripción del proyecto 4.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+    {
+      title: "Proyecto 5",
+      description: "Descripción del proyecto 5.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+    {
+      title: "Proyecto 6",
+      description: "Descripción del proyecto 6.",
+      image: "https://picsum.photos/1000/1000",
+      urlGithub: "",
+      urlDemo: "",
+      gallery: [
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+        "https://picsum.photos/1000/1000",
+      ],
+    },
+  ]
 
   return (
     <div className={st.page}>
-      <div className={"position-relative d-flex h-100vh"}>
+      <div className={"position-relative"}>
 
         <div className={`${st.infoMain} d-flex text-center align-c-c`}>
           <div className="container">
@@ -144,6 +220,18 @@ const Home = () => {
                   />
                 </a>
               ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="w-100">
+          <div className="container">
+            <div className={st.cardContainer}>
+              {
+                proyects.map((proyecto, index) => (
+                  <CardProject key={index} data={proyecto}/>
+                ))
+              }
             </div>
           </div>
         </div>
