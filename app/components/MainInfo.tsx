fa5bc51b1c89b31e91c4314a0dae9bcfbfe4ed85
GatekoNeko141 <jhon1995jera@gmail.com>
2025-07-16 12:54:08 -0500

    Insersion de datos en el DOM desde el servidor

--- app/components/MainInfo.tsx
+++ app/components/MainInfo.tsx
@@ -5,74 +5,7 @@ import Image from "next/image";
 import st from './MainInfo.module.scss';
 
 const MainInfo = () => {
-  console.log("===ENV===", process.env.NEXT_PUBLIC_API_BASE_URL)
-  
-  const temp:Data = use(getMainInfo());
-  console.log("getMainInfo()", temp)
-
-  const data:Data = {
-    image: 'https://picsum.photos/1000/1000',
-    nombre: "John Eymar",
-    apellido: "Rodríguez Arteaga",
-    subtitle: "Ingeniero Desarrollador Web Frontend",
-    description: "Desarrollador web con experiencia en tecnologías modernas como React, Next.js, Angular y Vue.js. Apasionado por crear interfaces de usuario intuitivas y eficientes.",
-    social: {
-      github: "https://github.com/GatekoNeko141",
-      linkedin: "https://www.linkedin.com/in/john-eymar-rodriguez/",
-    },
-    skills: [
-      {
-        name: "HTML5",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
-        url: "https://developer.mozilla.org/docs/Web/HTML",
-      },
-      {
-        name: "CSS3",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
-        url: "https://developer.mozilla.org/docs/Web/CSS",
-      },
-      {
-        name: "JavaScript",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
-        url: "https://developer.mozilla.org/docs/Web/JavaScript",
-      },
-      {
-        name: "TypeScript",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
-        url: "https://www.typescriptlang.org/",
-      },
-      {
-        name: "Next.js",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original.svg",
-        url: "https://nextjs.org/",
-      },
-      {
-        name: "React",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
-        url: "https://react.dev/",
-      },
-      {
-        name: "Angular",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg",
-        url: "https://angular.io/",
-      },
-      {
-        name: "Vue.js",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg",
-        url: "https://vuejs.org/",
-      },
-      {
-        name: "Node.js",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
-        url: "https://nodejs.org/",
-      },
-      {
-        name: "PHP",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg",
-        url: "https://www.php.net/",
-      },
-    ],
-  };
+  const data:Data = use(getMainInfo());
 
   const MainInfoHTML = (<>
     <div className={`${st.infoMain} d-flex text-center align-c-c`}>
